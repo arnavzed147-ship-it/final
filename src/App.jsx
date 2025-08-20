7fa834196d07dccb7e8725214ba3b2de5a74ae71
arnavzed147-ship-it <arnavzed147@gmail.com>
2025-08-20 13:18:45 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1194,3 +1194,8 @@ function MobileDisclosure({ label, children }) {
     </div>
   );
 }
+<form name="contact" method="POST" data-netlify="true" netlify-honeypot="bot-field" hidden>
+  <input type="text" name="name" />
+  <input type="email" name="email" />
+  <textarea name="message"></textarea>
+</form>
